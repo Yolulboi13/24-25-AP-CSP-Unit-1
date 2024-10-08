b1f6902bc4cdee24e4a2e3a80e939a2af2ebc7a0
Yolulboi13 <marcus.debarbieri@vtsd.com>
2024-10-08 13:45:33 -0400

    10/2/2024 1-1-7 happened messin around w lists

---
+++ 1-1-7/step1.py
@@ -0,0 +1,22 @@
+#list
+new_list = ["dog", "cat", "mouse", "bird", "monkey"]
+print(new_list)
+
+#print specific item
+print(new_list[2])
+
+#change specific item
+new_list[2]="dog"
+print(new_list)
+
+#loops
+for item in new_list:
+    print(item)
+
+#appending
+new_list.append("lion")
+print(new_list)
+
+#popping
+new_list.pop()
+print(new_list)
